ec5aeb82bd377041687a5d7ec3d759c9f2f13e4d
any0201 <anyychouhan143@gmail.com>
2024-06-27 13:37:32 +0530

    my first commit

--- Encapsulation.java
+++ Encapsulation.java
@@ -1,19 +1,40 @@
 public class Encapsulation {
 
-
     public static void main(String[] args) {
-        
+
         BankAcc b1 = new BankAcc();
-        b1.Accno=122322;
-        b1.name="anyy";
+        // b1.Accno=122322;
+        b1.setname("anyy");
+        b1.setAcc(1254932);
+        System.out.println(b1.getname());
+        System.out.println(b1.getAcc());
 
-        System.out.println("the name of acc holder is " + b1.name + " having acc no = " + b1.Accno);
+        // System.out.println("the name of acc holder is " + b1.name + " having acc no =
+        // " + b1.Accno);
     }
-    
-}
-//encapsulation
-class BankAcc{
-    int Accno;
-    String name;
 
+}
+
+// encapsulation
+class BankAcc {
+    // int Accno;
+    private String name;
+    private int Accno; // encapsulating
+
+    // setter - setting the value in the form of integer
+    void setAcc(int n) { //taking the input, using void because it has no return type. and declaring the value of vaariable
+        this.Accno = n; //fake variable
+    }
+
+    // getter - getting the vale of that variable
+    int getAcc() {
+        return this.Accno;
+
+    }
+    void setname(String n){
+        this.name=n;
+    }
+    String getname(){
+        return this.name;
+    }
 }
